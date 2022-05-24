d91191faaf03cb043138b126f939421e11d6e2e3
u21451088 <u21451088@tuks.co.za>
2022-05-24 19:07:59 +0200

    incorporated team-member to Toury page

--- Frontend/src/app/team/team.component.ts
+++ Frontend/src/app/team/team.component.ts
@@ -8,9 +8,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TeamComponent implements OnInit {
   teamName: string;
+  teamID: number;
 
   constructor(private route: ActivatedRoute,) {
     this.teamName = this.route.snapshot.paramMap.get('teamName');
+    this.teamID = Number(this.route.snapshot.paramMap.get('teamID'));
   }
 
   ngOnInit(): void {
