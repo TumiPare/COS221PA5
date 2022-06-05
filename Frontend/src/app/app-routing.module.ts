faf20996cecb535cf067846d193eba5736795713
u21451088 <u21451088@tuks.co.za>
2022-06-05 21:48:41 +0200

    changed "tournement' to "tournament" -spelling fix

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: '', component: EventsComponent },
   { path: 'team/:teamID/:teamName', component: TeamComponent },
   { path: 'player/:playerID/:playerName', component: PlayerComponent },
-  { path: 'tournement/:tournementID/:tournementName', component: TournementComponent },
+  { path: 'tournament/:tournementID/:tournementName', component: TournementComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'manage', component: ManageComponent }
