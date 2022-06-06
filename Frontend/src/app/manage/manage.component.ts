2c5534597bc449b28641421434b7b365699f60c0
u21451088 <u21451088@tuks.co.za>
2022-06-06 16:17:22 +0200

    implemented cookie apiKey storage on manage page

--- Frontend/src/app/manage/manage.component.ts
+++ Frontend/src/app/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { APIService } from '../api.service';
 
 @Component({
@@ -9,7 +10,8 @@ import { APIService } from '../api.service';
 export class ManageComponent implements OnInit {
 
   constructor(
-    private api: APIService
+    private api: APIService,
+    private router: Router,
   ) { }
 
   print(jan: string): void {
@@ -17,14 +19,11 @@ export class ManageComponent implements OnInit {
     document.getElementById("mess").innerHTML = "<p>" + jan + "</p>";
   }
 
-  Setter() :void  // Validate that give input is valid
-  {
-
-    let Password =  (<HTMLInputElement>document.getElementById("password")).value;
+  Setter(): void { // Validate that give input is valid
+    let Password = (<HTMLInputElement>document.getElementById("password")).value;
     let check = (<HTMLInputElement>document.getElementById("cpassword")).value;
 
-    if(Password!=check)   // confirm password wrong
-    {
+    if (Password != check) {  // confirm password wrong
       this.print("Password and confirmation do not match")
       return;
     }
@@ -34,103 +33,82 @@ export class ManageComponent implements OnInit {
     let Key = sessionStorage.getItem("apiKey");
 
     //Check for is empty
-
-
-    if(Username=="")
-    {
+    if (Username == "") {
       this.print("Please enter your username");
       return;
     }
 
-  //Did not change email
-  if(Email == sessionStorage.getItem("email")) Email = "";  //Refer to UpdateUser in api.service
+    //Did not change email
+    if (Email == sessionStorage.getItem("email")) Email = "";  //Refer to UpdateUser in api.service
 
-  if(Username == sessionStorage.getItem("username")) Username = "";  //Refer to UpdateUser in api.service
-
-
-
-  if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(Email)) {
-    // Bad email
-    document.getElementById("email").focus();
-    this.print("Please check that your email is valid");
-  } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm.test(Password)) {
-    // Bad Password
-    document.getElementById("password").focus();
-    this.print("Please ensure that your Password is valid and correct");
-  } 
-  
-  else {
-
-  this.api.UpdateUser( Email, Username,Password,Key).subscribe((res) => {
-    console.log(res);
+    if (Username == sessionStorage.getItem("username")) Username = "";  //Refer to UpdateUser in api.service
 
 
-    if(res.status=="success")
-    {
-      sessionStorage.setItem("username",res.data[0].username);
-      sessionStorage.setItem("email",res.data[0].email);
-      sessionStorage.setItem("apiKey",res.data[0].apiKey);
-      this.print("Account succesfully updated");
+
+    if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(Email)) {
+      // Bad email
+      document.getElementById("email").focus();
+      this.print("Please check that your email is valid");
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm.test(Password)) {
+      // Bad Password
+      document.getElementById("password").focus();
+      this.print("Please ensure that your Password is valid and correct");
     }
-    else
-      if(res.status=="failed")
-      {
-        // do something
-        if(res.error.code = "invalid_email")
-        {
-          this.print(res.error.message);
-          (<HTMLInputElement>document.getElementById("email")).focus;
-        }
 
-        if(res.error.code = "invalid_password")
-        {
-          this.print(res.error.message);
-          (<HTMLInputElement>document.getElementById("password")).focus;
-        }
-      }
-    else
-    this.print("Something went wrong");
-    
+    else {
 
-    });
+      this.api.UpdateUser(Email, Username, Password, Key).subscribe((res) => {
+        if (res.status == "success") {
+          sessionStorage.setItem("username", res.data[0].username);
+          sessionStorage.setItem("email", res.data[0].email);
+          sessionStorage.setItem("apiKey", res.data[0].apiKey);
+          this.api.setAPIKey(res.data[0].apiKey);
+          this.print("Account succesfully updated");
+        }
+        else
+          if (res.status == "failed") {
+            // do something
+            if (res.error.code = "invalid_email") {
+              this.print(res.error.message);
+              (<HTMLInputElement>document.getElementById("email")).focus;
+            }
+
+            if (res.error.code = "invalid_password") {
+              this.print(res.error.message);
+              (<HTMLInputElement>document.getElementById("password")).focus;
+            }
+          }
+          else
+            this.print("Something went wrong");
+      });
+    }
   }
-  }
-  
+
 
   ngOnInit(): void {
-
     (<HTMLInputElement>document.getElementById("email")).value = sessionStorage.getItem("email");
     (<HTMLInputElement>document.getElementById("username")).value = sessionStorage.getItem("username");
 
-    
-
-
-    document.getElementById("Deleteimg").onclick = () =>
-    {
-      if(confirm('Are you sure you want to delete this account? This decision is permanent'))
-      {
+    document.getElementById("Deleteimg").onclick = () => {
+      if (confirm('Are you sure you want to delete this account? This decision is permanent')) {
         let Key = sessionStorage.getItem("apiKey");
         this.api.DeleteUser(Key).subscribe((res) => {
           console.log(res);
-      
-      
-          if(res.status=="success")
-          {
+
+          if (res.status == "success") {
             alert("account deleted");
-            window.location.replace("https://faade.co.za/login");
+            this.router.navigate([`login`]);
           }
           else
-          this.print("Something went wrong");
-          
-      
-          });
+            this.print("Something went wrong");
+        });
       }
       else
-      return;
-    
+        return;
+
     }
   }
 
-  
+
 
 }
