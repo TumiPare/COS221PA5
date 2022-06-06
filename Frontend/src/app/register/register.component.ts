5532ffe3da45685941f1cf20313d71e3df2bbe20
u21451088 <u21451088@tuks.co.za>
2022-06-06 11:13:25 +0200

    lowercase password and registration works

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { response } from 'express';
+import { Router } from '@angular/router';
 import { APIService } from '../api.service';
 
 @Component({
@@ -11,7 +11,8 @@ export class RegisterComponent implements OnInit {
 
 
   constructor(
-    private api: APIService
+    private api: APIService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -31,23 +32,29 @@ export class RegisterComponent implements OnInit {
       // Bad email
       document.getElementById("email").focus();
       this.print("Please check that your email is valid");
-    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/gm.test(userPassword)) {
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]{8,}$/gm.test(userPassword)) {
       // Bad Password
-      document.getElementById("Password").focus();
+      document.getElementById("password").focus();
       this.print("Please ensure that your Password is valid and correct");
     } else {
       // All validations passed
       this.api.signUpUser(username, userEmail, userPassword).subscribe((res) => {
-        console.log(res);
-        // do something whether it is successful or not
+        if (res.status == "success") {
+          this.api.setAPIKey(res.data.apiKey);
+          this.router.navigate([``]);
+        } else {
+          // Bad Email
+          document.getElementById("email").focus();
+          this.print(res.data.message);
+        }
       });
     }
   }
 
   displayTooltip(): void {
     document.getElementsByClassName("bar")[0].innerHTML = "<br><p>Please enter your email and password. Ensure your password has a digit and special character</p>"
-      let jan = document.getElementsByClassName("bar") as HTMLCollectionOf<HTMLElement>;
-      jan[0].style.visibility = "visible";
+    let jan = document.getElementsByClassName("bar") as HTMLCollectionOf<HTMLElement>;
+    jan[0].style.visibility = "visible";
   }
 
 }
