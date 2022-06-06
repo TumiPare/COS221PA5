b171934ea068e6082935c31366e0c513e7eeeae1
u21451088 <u21451088@tuks.co.za>
2022-06-06 11:26:06 +0200

    OUR-OWN-ORIGIN header

--- Frontend/src/app/api.service.ts
+++ Frontend/src/app/api.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -9,11 +10,36 @@ export class APIService {
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+      'OUR-OWN-ORIGIN': environment.production ? 'faade.co.za' : 'localhost:4200',
     })
   };
   private apiURL = 'https://api.faade.co.za/API.php';
+  private apiKey: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.apiKey = this.getApiKey();
+  }
+
+  setAPIKey(apiKey: string, expiresInDays?: number): void {
+    let expires = "";
+    if (expiresInDays) {
+      var date = new Date();
+      date.setTime(date.getTime() + (expiresInDays * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = "apiKey" + "=" + (apiKey || "") + expires + "; path=/";
+  }
+
+  getApiKey(): string {
+    var nameEQ = "apiKey" + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 
   signUpUser(username: string, email: string, password: string): Observable<any> {
     let body = {
@@ -28,7 +54,7 @@ export class APIService {
     return this.http.post(this.apiURL, body, this.httpOptions);
   }
 
-  ValidateUser( email: string, password: string): Observable<any> {
+  ValidateUser(email: string, password: string): Observable<any> {
     let body = {
       type: "user",
       operation: "login",
