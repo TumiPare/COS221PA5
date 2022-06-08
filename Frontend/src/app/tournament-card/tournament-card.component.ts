9592be53a4946d84785aef93d0f8262d5aa463c4
u21451088 <u21451088@tuks.co.za>
2022-06-08 08:39:53 +0200

    final

--- Frontend/src/app/tournament-card/tournament-card.component.ts
+++ Frontend/src/app/tournament-card/tournament-card.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {Event} from 'src/app/tournement/event'
+import { Event } from 'src/app/tournement/event'
 @Component({
   selector: 'tournament-card',
   templateUrl: './tournament-card.component.html',
   styleUrls: ['./tournament-card.component.css']
-  
+
 })
 export class TournamentCardComponent implements OnInit {
-  @Input('event')event: Event;
+  @Input('event') event: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.event);
   }
 
 }
--- Frontend/src/app/tournement/tournement.component.ts
+++ Frontend/src/app/tournement/tournement.component.ts
@@ -31,144 +31,136 @@ export class TournementComponent implements OnInit {
     this.api.getTournament(this.tournamentID).subscribe((res) => {
       console.log(res);
       this.tournament = res.data.tournament;
-      for (let i = 0; i < 8; i++) {
-        let Team1 = res.data.tournament.rounds[0].matches[i].teamA.teamID;
-        let Team2 = res.data.tournament.rounds[0].matches[i].teamB.teamID;
+      // for (let i = 0; i < 8; i++) {
+      //   let Team1 = res.data.tournament.rounds[0].matches[i].teamA.teamID;
+      //   let Team2 = res.data.tournament.rounds[0].matches[i].teamB.teamID;
 
-        
+      //   this.api.get2Teams(Team1, Team2).subscribe((req) => {  // Get the teams
+      //     console.log(req);
 
-        console.log(Team1);
-        console.log(Team2); 
-        this.api.get2Teams(Team1, Team2).subscribe((req) => {  // Get the teams
-          console.log(req);
+      //     if (req.status == "success") {
+            
+      //       Team1 = req.teams[0].name;
+      //       let Team1pic = req.teams[0].teamLogo;
 
-          if (req.status == "success") {
-            Team1 = req.data[0].name;
-            let Team1pic = req.data[0].teamLogo;
+      //       Team2 = req.teams[1].name;
+      //       let Team2pic = req.teams[1].teamLogo;
 
-            Team2 = req.data[1].name;
-            let Team2pic = req.data[1].teamLogo;
+      //       this.tournament.roundof16[i].teamA = Team1;
+      //       this.tournament.roundof16[i].picA = Team1pic;
 
-            this.tournament.rounds[0].matches[i].teamA = Team1;
-            this.tournament.rounds[0].matches[i].picA = Team1pic;
-
-            this.tournament.rounds[0].matches[i].scoreA = res.data.tournament.rounds[0].matches[i].teamA.points;
+      //       this.tournament.roundof16[i].scoreA = res.data.rounds[0].matches[i].teamA.points;
 
 
-            this.tournament.rounds[0].matches[i].teamB = Team2;
-            this.tournament.rounds[0].matches[i].picB = Team2pic;
+      //       this.tournament.roundof16[i].teamB = Team2;
+      //       this.tournament.roundof16[i].picB = Team2pic;
 
-            this.tournament.rounds[0].matches[i].scoreB = res.data.tournament.rounds[0].matches[i].teamB.points;
-          }
-          else
-            console.log("Something went wrong with getTeam");
-        });
-      }
-      for (let i = 0; i < 4; i++) {
-        let Team1 = res.data.tournament.rounds[1].matches[i].teamA.teamID;
-        let Team2 = res.data.tournament.rounds[1].matches[i].teamB.teamID;
+      //       this.tournament.roundof16[i].scoreB = res.data.rounds[0].matches[i].teamB.points;
+      //     }
+      //     else
+      //       console.log("Something went wrong with getTeam");
+      //   });
+      // }
+      // for (let i = 0; i < 4; i++) {
+      //   let Team1 = res.data.tournament.rounds[1].matches[i].teamA.teamID;
+      //   let Team2 = res.data.tournament.rounds[1].matches[i].teamB.teamID;
 
-        
-        
+      //   this.api.get2Teams(Team1, Team2).subscribe((req) => { // Get the teams
+      //     console.log(req);
 
-        this.api.get2Teams(Team1, Team2).subscribe((req) => { // Get the teams
-          console.log(req);
+      //     if (req.status == "success") {
+      //       Team1 = req.teams[0].name;
+      //       let Team1pic = req.teams[0].teamLogo;
 
+      //       Team2 = req.teams[1].name;
+      //       let Team2pic = req.teams[1].teamLogo;
 
+      //       this.tournament.quarterFinals[i].teamA = Team1;
+      //       this.tournament.quarterFinals[i].picA = Team1pic;
 
-          if (req.status == "success") {
-            Team1 = req.data[0].name;
-            let Team1pic = req.data[0].teamLogo;
-
-            Team2 = req.data[1].name;
-            let Team2pic = req.data[1].teamLogo;
-
-            this.tournament.quarterFinals[i].teamA = Team1;
-            this.tournament.quarterFinals[i].picA = Team1pic;
-
-            this.tournament.quarterFinals[i].scoreA = res.data.tournament.rounds[1].matches[i].teamA.points;
+      //       this.tournament.quarterFinals[i].scoreA = res.data.rounds[1].matches[i].teamA.points;
 
 
-            this.tournament.quarterFinals[i].teamB = Team2;
-            this.tournament.quarterFinals[i].picB = Team2pic;
+      //       this.tournament.quarterFinals[i].teamB = Team2;
+      //       this.tournament.quarterFinals[i].picB = Team2pic;
 
-            this.tournament.quarterFinals[i].scoreB = res.data.tournament.rounds[1].matches[i].teamB.points;
-          }
-          else
-            console.log("Something went wrong with getTeam");
+      //       this.tournament.quarterFinals[i].scoreB = res.data.rounds[1].matches[i].teamB.points;
+      //     }
+      //     else
+      //       console.log("Something went wrong with getTeam");
 
-        });
+      //   });
 
-      }
+      // }
 
-      for (let i = 0; i < 2; i++) {
+      // for (let i = 0; i < 2; i++) {
 
-        let Team1 = res.data.tournament.rounds[2].matches[i].teamA.teamID;
-        let Team2 = res.data.tournament.rounds[2].matches[i].teamB.teamID;
-
-
-        this.api.get2Teams(Team1, Team2).subscribe((req) => {                 //Get the teams
-          console.log(req);
-
-          if (req.status == "success") {
-            Team1 = req.data[0].name;
-            let Team1pic = req.data[0].teamLogo;
-
-            Team2 = req.data[1].name;
-            let Team2pic = req.data[1].teamLogo;
-
-            this.tournament.semiFinals[i].teamA = Team1;
-            this.tournament.semiFinals[i].picA = Team1pic;
-
-            this.tournament.semiFinals[i].scoreA = res.data.tournament.rounds[2].matches[i].teamA.points;
+      //   let Team1 = res.data.tournament.rounds[2].matches[i].teamA.teamID;
+      //   let Team2 = res.data.tournament.rounds[2].matches[i].teamB.teamID;
 
 
-            this.tournament.semiFinals[i].teamB = Team2;
-            this.tournament.semiFinals[i].picB = Team2pic;
+      //   this.api.get2Teams(Team1, Team2).subscribe((req) => {                 //Get the teams
+      //     console.log(req);
 
-            this.tournament.semiFinals[i].scoreB = res.data.tournament.rounds[2].matches[i].teamB.points;
-          }
-          else
-            console.log("Something went wrong with getTeam");
+      //     if (req.status == "success") {
+      //       Team1 = req.teams[0].name;
+      //       let Team1pic = req.teams[0].teamLogo;
 
-        });
+      //       Team2 = req.teams[1].name;
+      //       let Team2pic = req.teams[1].teamLogo;
 
-      }
+      //       this.tournament.semiFinals[i].teamA = Team1;
+      //       this.tournament.semiFinals[i].picA = Team1pic;
 
-
-      {
-
-        let Team1 = res.data.tournament.rounds[3].matches[0].teamA.teamID;
-        let Team2 = res.data.tournament.rounds[3].matches[0].teamB.teamID;
+      //       this.tournament.semiFinals[i].scoreA = res.data.rounds[2].matches[i].teamA.points;
 
 
-        this.api.get2Teams(Team1, Team2).subscribe((req) => {                 //Get the teams
-          console.log(req);
+      //       this.tournament.semiFinals[i].teamB = Team2;
+      //       this.tournament.semiFinals[i].picB = Team2pic;
 
-          if (res.status == "success") {
-            Team1 = req.teams[0].name;
-            let Team1pic = req.teams[0].teamLogo;
+      //       this.tournament.semiFinals[i].scoreB = res.data.rounds[2].matches[i].teamB.points;
+      //     }
+      //     else
+      //       console.log("Something went wrong with getTeam");
 
-            Team2 = req.teams[1].name;
-            let Team2pic = req.teams[1].teamLogo;
+      //   });
 
-            this.tournament.final.teamA = Team1;
-            this.tournament.final.picA = Team1pic;
-
-            this.tournament.final.scoreA = res.data.tournament.rounds[3].matches[0].teamA.points
+      // }
 
 
-            this.tournament.final.teamB = Team2;
-            this.tournament.final.picB = Team2pic;
+      // {
 
-            this.tournament.final.scoreB = res.data.tournament.rounds[3].matches[0].teamB.points;
-          }
-          else
-            console.log("Something went wrong with getTeam");
+      //   let Team1 = res.data.tournament.rounds[3].matches[0].teamA.teamID;
+      //   let Team2 = res.data.tournament.rounds[3].matches[0].teamB.teamID;
 
-        });
 
-      }
+      //   this.api.get2Teams(Team1, Team2).subscribe((req) => {                 //Get the teams
+      //     console.log(req);
+
+      //     if (res.status == "success") {
+      //       Team1 = req.teams[0].name;
+      //       let Team1pic = req.teams[0].teamLogo;
+
+      //       Team2 = req.teams[1].name;
+      //       let Team2pic = req.teams[1].teamLogo;
+
+      //       this.tournament.final.teamA = Team1;
+      //       this.tournament.final.picA = Team1pic;
+
+      //       this.tournament.final.scoreA = res.data.rounds[3].matches[0].teamA.points
+
+
+      //       this.tournament.final.teamB = Team2;
+      //       this.tournament.final.picB = Team2pic;
+
+      //       this.tournament.final.scoreB = res.data.rounds[3].matches[0].teamB.points;
+      //     }
+      //     else
+      //       console.log("Something went wrong with getTeam");
+
+      //   });
+
+      // }
 
 
       //  
