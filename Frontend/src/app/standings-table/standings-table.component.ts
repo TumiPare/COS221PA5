8c5c9b03777fa89e4b3aeaa1842c8d6e073e03b9
Tumi Pare <u221452832@tuks.co.za>
2022-06-08 02:35:36 +0200

    Small updates to tournament page variables

--- Frontend/src/app/standings-table/standings-table.component.ts
+++ Frontend/src/app/standings-table/standings-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 export interface Standing {
   pos: number;
@@ -19,11 +19,12 @@ export interface Standing {
   styleUrls: ['./standings-table.component.css']
 })
 export class StandingsTableComponent implements OnInit {
-  dataSource: Standing[] = [
-    { pos: 3, team: "Germany", Pld: 3, W: 2, D: 0, L: 1, GF: 35, GA: 29, GD: "+6", Pts: 4 },
-    { pos: 3, team: "Hungary", Pld: 3, W: 2, D: 0, L: 1, GF: 38, GA: 29, GD: "+4", Pts: 4 },
-    { pos: 3, team: "Australia", Pld: 3, W: 2, D: 0, L: 1, GF: 32, GA: 29, GD: "+8", Pts: 4 },
-    { pos: 3, team: "Japan", Pld: 3, W: 0, D: 0, L: 3, GF: 35, GA: 47, GD: "18", Pts: 0 },
+  @Input('standings') standings1: Standing;
+  standings: Standing[] = [
+    { "pos": 3, "team": "Germany", "Pld": 3, "W": 2, "D": 0, "L": 1, "GF": 35, "GA": 29, "GD": "+6", "Pts": 4 },
+    { "pos": 3, "team": "Hungary", "Pld": 3, "W": 2, "D": 0, "L": 1, "GF": 38, "GA": 29, "GD": "+4", "Pts": 4 },
+    { "pos": 3, "team": "Australia", "Pld": 3, "W": 2, "D": 0, "L": 1, "GF": 32, "GA": 29, "GD": "+8", "Pts": 4 },
+    { "pos": 3, "team": "Japan", "Pld": 3, "W": 0, "D": 0, "L": 3, "GF": 35, "GA": 47, "GD": "18", "Pts": 0 },
   ];
   displayedColumns: string[] = ['pos', 'team', 'Pld', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts'];
   constructor() { }
