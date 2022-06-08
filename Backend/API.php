be404e073db6de54548aef495e33a2848dd45d32
GD Radloff <danielrad082@gmail.com>
2022-06-08 07:13:24 +0200

    added a way to get all the events for a tournament

--- Backend/API.php
+++ Backend/API.php
@@ -452,6 +452,7 @@ class API {
     }
 
     private function getMatches($data) {
+	$this->response = $this->database->getMatches($data[0]["tournamentID"]);
     }
 
     // ===================LEAGUES===================
